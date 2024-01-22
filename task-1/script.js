0f0ff90231cf61602dac5c5cac90140c29e3b09a
Alina Nadtochiy <am0817@mail.ru>
2024-01-22 14:38:21 +0300

    img onclick

---
+++ task-1/script.js
@@ -0,0 +1,11 @@
+// !function() {
+//     alert("Hello from IIFE!");
+// }();
+
+// void function() {
+//     alert("Hello from IIFE!");
+// }(); 
+const image = document.getElementById('image01');
+function openImage() {
+    image.src = './assets/images/image01.jpg'
+};
