7d6fea470b0a96527ff15f4a83fa9d20f1aeace7
Alina Nadtochiy <am0817@mail.ru>
2024-01-23 14:57:51 +0300

    task 3 week 10

--- task-1/Week10-2-3/script.js
+++ task-1/Week10-2-3/script.js
@@ -2,3 +2,10 @@ function greet() {
     console.log('Я учу JavaScript!');
 };
 greet();
+let cat = document.getElementById('cat');
+function secondImage() {
+    cat.src = './assets/images/cat-06.jpg';
+};
+function firstImage() {
+    cat.src = './assets/images/cat-01.jpg';
+};
