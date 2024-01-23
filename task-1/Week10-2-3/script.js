142522bc1f4a6ccb7b95f7cd1c52372fcaaa81b0
Alina Nadtochiy <am0817@mail.ru>
2024-01-23 17:07:19 +0300

    task 4* week 10

--- task-1/Week10-2-3/script.js
+++ task-1/Week10-2-3/script.js
@@ -1,7 +1,12 @@
+// Практическое задание 2
+
 function greet() {
     console.log('Я учу JavaScript!');
 };
 greet();
+
+// Практическое задание 3
+
 let cat = document.getElementById('cat');
 function nextImage() {
     cat.src = './assets/images/cat-06.jpg';
@@ -10,16 +15,29 @@ function prevImage() {
     cat.src = './assets/images/cat-01.jpg';
 };
 
+// *** Задание под звёздочкой: вариант результатов с помощью ALERT
+
 // function calculateTotalPrice(quantity=2, price=500) {
 // const total = quantity*price;
 // alert(`Стоимость покупки: ${total}₽`)
 // }
 // calculateTotalPrice(5, 1300);
 
+// вариант результатов с помощью вывода сообщения 
+
 const totalPrice = document.getElementById('text');
 function calculateTotalPrice(quantity=2, price=500) {
     const total = quantity*price;
     totalPrice.textContent = `Стоимость покупки: ${total.toLocaleString('ru-RU')} ₽`;
-    return total;
 };
-// calculateTotalPrice(5, 1300);
+// calculateTotalPrice(5, 1300);  // пример с заданными параметрами
+
+// *** Задание под звёздочкой: вариант введения значений пользователем
+
+const totalCost = document.getElementById('value');
+function calculateTotalCost() {
+    const qt = document.getElementById('qt').value;
+    const pr = document.getElementById('pr').value;
+    const cost = qt*pr;
+    totalCost.textContent = `Стоимость покупки: ${cost.toLocaleString('ru-RU')} ₽`;
+}
