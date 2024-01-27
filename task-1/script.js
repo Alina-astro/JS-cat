7a7317d9be259fa1a568c5f01aa6abc5d00f8b2c
Alina Nadtochiy <am0817@mail.ru>
2024-01-27 21:34:49 +0300

    test week 11

--- task-1/script.js
+++ task-1/script.js
@@ -5,7 +5,28 @@
 // void function() {
 //     alert("Hello from IIFE!");
 // }(); 
+
+// ------------Изменяет заголовок страницы---------------------------//
+// const headingElement = document.getElementById("main-heading");
+// 		console.log(headingElement.innerHTML);
+// 		const newHeadingText = prompt("Введите новый заголовок:");
+// 		headingElement.innerHTML = newHeadingText;
+
+// ------------Изменяет тему страницы---------------------------//
+const bgBody = document.getElementById('body');
+function changeBgDay() {
+    bgBody.style.backgroundColor = 'rgba(86, 32, 50, 0.603)';
+}
+function changeBgNight() {
+    bgBody.style.backgroundColor = 'rgba(32, 2, 26, 0.937)';
+}
+// ------------Изменяет картинку при нажатии кнопки---------------------------//
 const image = document.getElementById('image01');
 function openImage() {
     image.src = './assets/images/image01.jpg'
 };
+// ------------Изменяется цвет кнопки----------------------------------------//
+function changeBtn() {
+    const btn = document.getElementById('button-test-toggle');
+    btn.classList.toggle('maroon');
+}
