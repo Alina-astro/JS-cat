6e0f37bf25a99bf77e844c14a4abf49e6263df81
Alina Nadtochiy <am0817@mail.ru>
2024-04-26 09:05:53 +0300

    change variables, add calc for total sum

--- task-1/Week11/w11-task3/script.js
+++ task-1/Week11/w11-task3/script.js
@@ -1,11 +1,44 @@
+let priceDeserts = 2250;
+let pricePizza = 2970;
+let priceRolls = 4580;
+let priceSushi = 3520;
+
 const costDeserts = document.getElementById('costDeserts');
-const menu1 = Number(costDeserts.textContent);
+costDeserts.textContent = `${priceDeserts.toLocaleString('ru-RU')} ₽`;
+
 const costPizza = document.getElementById('costPizza');
-const menu2 = Number(costPizza.textContent);
+costPizza.textContent = `${pricePizza.toLocaleString('ru-RU')} ₽`;
+
 const costRolls = document.getElementById('costRolls');
-const menu3 = Number(costRolls.textContent);
+costRolls.textContent = `${priceRolls.toLocaleString('ru-RU')} ₽`;
+
 const costSushi = document.getElementById('costSushi');
-const menu4 = Number(costSushi.textContent);
+costSushi.textContent = `${priceSushi.toLocaleString('ru-RU')} ₽`;
+
 const totalValue = document.getElementById('totalValue');
-const cost = menu1+menu2+menu3+menu4;
-totalValue.textContent = `${cost.toLocaleString('ru-RU')} ₽`;
+const totalCost = priceDeserts + pricePizza + priceRolls + priceSushi;
+console.log(totalCost);
+totalValue.textContent = `${totalCost.toLocaleString('ru-RU')} ₽`;
+// function calculateTotalCost() {
+//     const totalCost = priceDeserts + pricePizza + priceRolls + priceSushi;
+//     console.log(totalCost);
+//     totalValue.textContent = `${totalCost.toLocaleString('ru-RU')} ₽`;
+//     return totalCost;
+// }
+// calculateTotalCost();
+
+const discValue = document.getElementById('discValue');
+const addDisc = document.getElementById('addDisc');
+
+addDisc.addEventListener('click', function() {
+    let discCost = totalCost - totalCost*0.2;
+    discValue.textContent = `Итого с учётом скидки: ${discCost.toLocaleString('ru-RU')} ₽`;
+    totalValue.style.textDecoration = 'line-through';
+    totalValue.style.color = 'grey';
+    addDisc.style.backgroundColor = 'rgba(202, 202, 202, 0.2)';
+})
+
+
+
+
+
