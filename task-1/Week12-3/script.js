9620a7ddb255a12abd5af429f001a196a0677949
Alina Nadtochiy <am0817@mail.ru>
2024-05-13 23:20:24 +0300

    task 12-3 add

---
+++ task-1/Week12-3/script.js
@@ -0,0 +1,5 @@
+const result = document.querySelector('.result');
+const button = document.querySelector('.btn');
+function toCreateNewWord() {
+    const alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+}
