52cbd4389e84e9cad9b4cb4f00c1ade518308953
Alina Nadtochiy <am0817@mail.ru>
2024-05-13 23:41:41 +0300

    task 12-3 done

--- task-1/Week12-3/script.js
+++ task-1/Week12-3/script.js
@@ -2,4 +2,10 @@ const result = document.querySelector('.result');
 const button = document.querySelector('.btn');
 function toCreateNewWord() {
     const alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+    const randomIndexOne = Math.floor(Math.random() * alphabet.length);
+    const randomIndexTwo = Math.floor(Math.random() * alphabet.length);
+    const randomIndexThree = Math.floor(Math.random() * alphabet.length);
+    const randomIndexFour = Math.floor(Math.random() * alphabet.length);
+    return result.innerHTML = alphabet[randomIndexOne]+alphabet[randomIndexTwo]+alphabet[randomIndexThree]+alphabet[randomIndexFour];
 }
+button.onclick = toCreateNewWord;
