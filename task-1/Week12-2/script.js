05f233f0d9b6cba587e24063729d6a0327849dd9
Alina Nadtochiy <am0817@mail.ru>
2024-05-15 22:20:54 +0300

    task 12-all some changes

--- task-1/Week12-2/script.js
+++ task-1/Week12-2/script.js
@@ -1,11 +1,11 @@
 const poem = document.querySelector('.poem');
 const text = poem.innerHTML;
 console.log(`1. Количество символов в тексте: ${text.length}`);
-const result = text.replace(/;/g, '; \n');
+const result = text.replace(/;/g, ';\n');  // ---- или использовать replaceAll(';', ';\n')
 console.log(`2. Текст с переносами строк: \n ${result}`);
 const substring = result.replace(/\s/g, '');
 console.log(`3. Текст с удалёнными пробелами: ${substring}`);
-const newText = result.substring(28,49);
+const newText = result.substring(28,50);
 console.log(`4. Извлечение подстроки: ${newText}`);
 const replacedText = newText.replace('клёне', 'дубе');
 console.log(`5. Замена "клёне" на "дубе": ${replacedText}`);
@@ -18,10 +18,12 @@ console.log(`8. Индекс первого вхождения "моря": ${ind
 const modifiedText = replacedText.replace(replacedText[0], replacedText[0].toUpperCase());
 console.log(`9. Строка с измененной первой буквой: ${modifiedText}`);
 function changeAllText() {
-    const change1 = text.replace(/;/g, ';\n'); // -------------------------------------------перенос строк не работает
+    const change1 = text.replace(/;/g, ';</br>'); // -------- перенос строк на странице делать через тег (т.к. это вёрстка)
     const change2 = change1.replace(/клён/g, 'дуб');
     const change3 = change2.replace(change2[0], change2[0].toUpperCase());
-    const change4 = change3.replace(('лукоморья','бабою','ягой','кащей'), ('Лукоморья','Бабою','Ягой','Кащей')); // -----меняет только последнее слово
-    console.log(change4);
-    return poem.textContent = change4;
+    const change4 = change3.replace(change3[2], change3[2].toUpperCase());
+    const change5 = change4.replace('бабою ягой', 'Бабою Ягой'); 
+    const change6 = change5.replace('кащей', 'Кащей'); 
+    const change7 = change6.replace('русью', 'Русью'); 
+    return poem.innerHTML = change7; // --------------------- для отображения на странице HTML используем innerHTML
 }
--- task-1/Week12-4/script.js
+++ task-1/Week12-4/script.js
@@ -20,11 +20,9 @@ button.addEventListener('click', function() {
     const userName = document.createElement('div'); // ------- создаём блок div для имени и добавляем стили
     userName.classList.add('user-comment__name');
     const name = newUserName.value;  // ---------------------- берём содержимое с именем
-    const nameWithoutSpaces = name.trim();  // --------------- убираем пробелы в имени
-    const nameLowCase = nameWithoutSpaces.toLowerCase(); // -- делаем все буквы строчными
-    const nameWithoutFirst = nameLowCase.slice(1);  // ------- отрезаем слово со 2 символа
-    const firstUpCase = nameWithoutSpaces[0].toUpperCase(); // делаем первую букву заглавной
-    userName.textContent = firstUpCase+nameWithoutFirst; // -- склеиваем первую букву и отрезанное слово - добавляем в содержание блока имени
+    const nameTrim = name.trim();  // --------------- убираем пробелы в имени
+    userName.textContent = nameTrim[0].toUpperCase()+nameTrim.slice(1).toLowerCase(); 
+    //добавляем в содержание блока имени: склеенные первую заглавную букву и отрезанные оставшиеся строчные 
     newBlockComment.append(userName); // --------------------- добавляем блок с именем
     
     const userComment = document.createElement('div'); // ---- создаём блок div для комментария
