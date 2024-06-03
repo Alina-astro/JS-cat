79373e8091fc1d9566400a36be47f9a6407a9518
Alina Nadtochiy <am0817@mail.ru>
2024-06-03 22:37:10 +0300

    task 14-3 add html, body, styles, array

---
+++ task-1/Week14-3/script.js
@@ -0,0 +1,56 @@
+const array = [
+    {
+      name: 'Стивен Спилберг',
+      career: 'Продюсер, Режиссёр, Сценарист, Актёр, Монтажёр',
+      films: 'https://www.film.ru/person/stiven-spilberg',
+      top_rated_film: 'Список Шиндлера'
+    },
+    {
+      name: 'Кристофер Нолан',
+      career: 'Продюсер, Режиссёр, Сценарист, Актёр, Монтажёр, Оператор, Композитор',
+      films: 'https://www.film.ru/person/kristofer-nolan',
+      top_rated_film: 'Начало'
+    },
+    {
+      name: 'Джордж Лукас',
+      career: 'Продюсер, Режиссёр, Сценарист, Актёр, Монтажёр, Оператор',
+      films: 'https://www.film.ru/person/dzhordzh-lukas',
+      top_rated_film: 'Звёздные войны: Эпизоды 1-6'
+    },
+    {
+      name: 'Дени Вильнёв',
+      career: 'Продюсер, Режиссёр, Сценарист, Актёр',
+      films: 'https://www.film.ru/person/deni-vilnev',
+      top_rated_film: 'Дюна'
+    },
+    {
+      name: 'Квентин Тарантино',
+      career: 'Продюсер, Режиссёр, Сценарист, Актёр, Монтажёр, Оператор',
+      films: 'https://www.film.ru/person/kventin-tarantino',
+      top_rated_film: 'Криминальное чтиво'
+    },
+    {
+      name: 'Питер Джексон',
+      career: 'Продюсер, Режиссёр, Сценарист, Актёр, Монтажёр, Оператор, Художник',
+      films: 'https://www.film.ru/person/piter-dzhekson',
+      top_rated_film: 'Властелин колец: Трилогия'
+    },
+    {
+      name: 'Джеймс Кэмерон',
+      career: 'Продюсер, Режиссёр, Сценарист, Актёр, Монтажёр, Оператор, Художник',
+      films: 'https://www.film.ru/person/dzheyms-kemeron',
+      top_rated_film: 'Терминатор 2: Судный день'
+    },
+    {
+        name: 'Гай Ричи',
+        career: 'Продюсер, Режиссёр, Сценарист, Актёр',
+        films: 'https://www.film.ru/person/gay-richi',
+        top_rated_film: 'Джентельмены'
+      },
+      {
+        name: 'Мартин Скорсезе',
+        career: 'Продюсер, Режиссёр, Сценарист, Актёр, Монтажёр, Оператор',
+        films: 'https://www.film.ru/person/martin-skorseze',
+        top_rated_film: 'Волк с Уолл-стрит'
+      },
+  ];
