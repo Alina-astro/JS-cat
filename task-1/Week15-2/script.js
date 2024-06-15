5b0a46a146a1626622db7875c4bb967b214dd7f0
Alina Nadtochiy <am0817@mail.ru>
2024-06-15 21:35:12 +0300

    task 15-2 done

--- task-1/Week15-2/script.js
+++ task-1/Week15-2/script.js
@@ -1,4 +1,5 @@
-const cities = [
+//----------------------------------------------- создаём массив с городами
+const CITIES = [
   "Milan",
   "Tokyo",
   "Moscow",
@@ -6,3 +7,55 @@ const cities = [
   "San Francisco",
   "Singapore",
 ];
+//----------------------------------------------- создаём пустой массив для температуры городов
+let WEATHER = [];
+//----------------------------------------------- создаём окно пользователя для введения температуры для городов
+for (let city of CITIES) {
+  let temp = prompt(`Enter the temperature for the city ${city}`);
+  WEATHER.push(Number(temp));
+}
+
+//----------------------------------------------- добавляем блок для выведения информации на экран
+const wrap = document.querySelector(".wrap");
+const divWeather = document.createElement("div");
+wrap.append(divWeather);
+
+//----------------------------------------------- добавляем в блок параграфы с городами и их температурой (с условием добавлять "+" для температуры выше 0)
+const getWeather = () => {
+  for (i = 0; i < CITIES.length; i++) {
+    let temp;
+    if (WEATHER[i] > 0) {
+      temp = `+${WEATHER[i]}`;
+    } else {
+      temp = `${WEATHER[i]}`;
+    }
+    divWeather.insertAdjacentHTML(
+      "beforebegin",
+      `<p>${CITIES[i]}: ${temp}&deg;C</p>`
+    );
+  }
+};
+getWeather(); //-----------------------------------------------//
+
+const getMinMaxWeather = () => {
+  //----------------------------------------------- вычисляем max и min температуры
+  const maxTemp = Math.max.apply(Math, WEATHER);
+  const minTemp = Math.min.apply(Math, WEATHER);
+
+  //----------------------------------------------- добавляем блок для max и min температур
+  const box = document.createElement("div");
+  box.classList.add("box");
+  divWeather.append(box);
+
+  //----------------------------------------------- добавляем строки в блоке для max и min температур
+  box.insertAdjacentHTML(
+    "afterbegin",
+    `<p>Minimum temperature: ${minTemp}&deg;C</p>`
+  );
+
+  box.insertAdjacentHTML(
+    "afterbegin",
+    `<p>Maximum temperature: ${maxTemp}&deg;C</p>`
+  );
+};
+getMinMaxWeather(); //-----------------------------------------------//
