392572ff873d01a0cd9a4c642174acb6e5284e2e
Alina Nadtochiy <am0817@mail.ru>
2024-07-28 02:28:22 +0300

    task 16-3 js, calc, carOwners

--- task-1/week16-3/script.js
+++ task-1/week16-3/script.js
@@ -1,11 +1,23 @@
 const formCarPrice = document.forms.carPrice;
 const carBrand = formCarPrice.elements.brand;
-const carModel = formCarPrice.elements.model;
+// const carModel = formCarPrice.elements.model;
 
 const carModelAudi = document.getElementById("carModelAudi");
 const carModelBmw = document.getElementById("carModelBmw");
 const carModelMercedes = document.getElementById("carModelMercedes");
 const carModelPorsche = document.getElementById("carModelPorsche");
+
+const carVolume = formCarPrice.elements.volume;
+const carMotor = formCarPrice.elements.motor;
+const typeCar = formCarPrice.elements.typeCar;
+const carType = document.querySelector(".carType");
+
+const divOwners = document.createElement("div");
+const divInputsOwners = document.createElement("div");
+const divBroken = document.createElement("div");
+
+const getPriceButton = document.getElementById("getPriceButton");
+let totalPrice = 2000000;
 
 carBrand.addEventListener("change", function () {
   chooseCarModel();
@@ -43,25 +55,18 @@ function chooseCarModel() {
   }
 }
 
-const carVolume = formCarPrice.elements.volume;
-const carMotor = formCarPrice.elements.motor;
-const carType = document.querySelector(".carType");
-
-const divOwners = document.createElement("div");
-// const divInputsOwners = document.createElement("div");
-const divBroken = document.createElement("div");
-
 function addBlockOwners() {
   carType.after(divOwners);
   divOwners.classList.add("checkbox-wrap");
   divOwners.innerHTML = `<p>Владельцев по ПТС</p>
-  <div class="checkbox-wrap__radio">
-      <input type="radio" id="one-two" name="owners" value="one-two" checked />
-      <label for="one-two">1-2</label>
-      <input type="radio" id="more_three" name="owners" value="more_three" />
-      <label for="more three">более 3х</label>
-  </div>`;
+    <div class="checkbox-wrap__radio">
+        <input type="radio" id="one-two" name="owners" value="one-two" checked />
+        <label for="one-two">1-2</label>
+        <input type="radio" id="more_three" name="owners" value="more_three" />
+        <label for="more three">более 3х</label>
+    </div>`;
 
+  return formCarPrice.elements.owners;
   // закомментированный код заменен на divOwners.innerHTML
 
   //   const labelOwners = document.createElement("p");
@@ -88,6 +93,7 @@ function addBlockOwners() {
 //   inputMoreThree.setAttribute("id", "more_three");
 //   inputMoreThree.setAttribute("name", "owners");
 //   inputMoreThree.setAttribute("value", "more_three");
+//   inputMoreThree.setAttribute("checked", "true");
 //   const labelMoreThree = document.createElement("label");
 //   labelMoreThree.setAttribute("for", "more_three");
 //   labelMoreThree.textContent = "более 3х";
@@ -115,6 +121,8 @@ function showOptions(e) {
       divBroken.innerHTML = "";
       addBlockBroken();
       addBlockOwners();
+      const owners = formCarPrice.elements.owners;
+      console.log(owners.value);
       //   addInputOwners();
     } else {
       divOwners.remove();
@@ -122,9 +130,130 @@ function showOptions(e) {
       divBroken.remove();
     }
   }
+  return formCarPrice.elements.owners;
 }
 
 const carTypeButtons = document.querySelectorAll('input[name="typeCar"]');
 for (const carTypeRadio of carTypeButtons) {
   carTypeRadio.addEventListener("change", showOptions);
 }
+
+const carCalcAudi = () => {
+  switch (carModelAudi.value) {
+    case "A6":
+      totalPrice += 2000000;
+      break;
+    case "A8":
+      totalPrice += 4000000;
+      break;
+    case "Q5":
+      totalPrice += 6000000;
+      break;
+    case "e-tron":
+      totalPrice += 8000000;
+      break;
+  }
+  if (carVolume.value >= 1.1 && carVolume.value < 2.0) {
+    totalPrice += 500000;
+  } else if (carVolume.value >= 2.0 && carVolume.value < 3.0) {
+    totalPrice += 1000000;
+  } else {
+    totalPrice += 2000000;
+  }
+  switch (carMotor.value) {
+    case "petrol":
+      totalPrice += 100000;
+      break;
+    case "diesel":
+      totalPrice += 1000000;
+      break;
+    case "electric":
+      totalPrice += 5000000;
+      break;
+  }
+  switch (typeCar.value) {
+    case "new":
+      totalPrice += 1000000;
+      break;
+    case "used":
+      const carOwners = formCarPrice.elements.owners;
+      console.log(carOwners.value);
+      if (carOwners.value === "one-two") {
+        totalPrice -= 3000000;
+      } else {
+        totalPrice -= 5000000;
+      }
+      break;
+  }
+};
+
+function carCalc() {
+  switch (carBrand.value) {
+    case "Audi":
+      carCalcAudi();
+      break;
+
+    case "BMW":
+      switch (carModelBmw.value) {
+        case "M5":
+          totalPrice += 3000000;
+          break;
+        case "X5":
+          totalPrice += 5000000;
+          break;
+        case "iX":
+          totalPrice += 7000000;
+          break;
+        case "5 серия GT":
+          totalPrice += 10000000;
+          break;
+      }
+      break;
+
+    case "Mercedes-Benz":
+      switch (carModelMercedes.value) {
+        case "E-class":
+          totalPrice += 8000000;
+          break;
+        case "Maybach S-class":
+          totalPrice += 11000000;
+          break;
+        case "GLC-class":
+          totalPrice += 9000000;
+          break;
+        case "EQS":
+          totalPrice += 12000000;
+          break;
+      }
+      break;
+
+    case "Porsche":
+      switch (carModelPorsche.value) {
+        case "Panamera":
+          totalPrice += 10000000;
+          break;
+        case "Cayenne":
+          totalPrice += 13000000;
+          break;
+        case "Macan":
+          totalPrice += 15000000;
+          break;
+        case "Taycan AT":
+          totalPrice += 17000000;
+          break;
+      }
+      break;
+  }
+  return totalPrice;
+}
+
+getPriceButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  console.log(carBrand.value);
+  console.log(carVolume.value);
+  console.log(carMotor.value);
+  console.log(typeCar.value);
+  //   console.log(carOwners.value);
+
+  console.log(totalPrice);
+});
